52230a0a6ee62c9c2ebfda561583d74aeb6fcf19
qzhang503 <45829450+qzhang503@users.noreply.github.com>
2019-05-03 10:19:53 -0500

    Update proteoQ-corrplots.R

--- R/proteoQ-corrplots.R
+++ R/proteoQ-corrplots.R
@@ -2,67 +2,67 @@
 #'
 #' @import stringr dplyr ggplot2 GGally rlang
 #' @importFrom magrittr %>%
-plotCorr <- function (df = NULL, col_select = NULL, col_order = NULL, 
-                      label_scheme_sub = label_scheme_sub, use_log10 = TRUE, 
-                      scale_log2r = scale_log2r, min_int = min_int, max_int = max_int, 
+plotCorr <- function (df = NULL, col_select = NULL, col_order = NULL,
+                      label_scheme_sub = label_scheme_sub, use_log10 = TRUE,
+                      scale_log2r = scale_log2r, min_int = min_int, max_int = max_int,
                       min_log2r = min_log2r, max_log2r = max_log2r,
                       filepath = filepath, filename = filename, ...) {
-	
+
 	dots <- rlang::enexprs(...)
-	
+
 	col_select <- rlang::enexpr(col_select)
 	col_order <- rlang::enexpr(col_order)
 
 	fn_prx <- gsub("\\..*$", "", filename)
 
-	if (use_log10) df$Intensity <- log10(df$Intensity) 
-	
+	if (use_log10) df$Intensity <- log10(df$Intensity)
+
 	if(dplyr::n_distinct(label_scheme_sub$Order) == 1) {
 		df$Intensity <- df$Intensity[, order(names(df$Intensity))]
 		df$log2R <- df$log2R[, order(names(df$log2R))]
 	} else {
-		corrplot_orders <- label_scheme_sub %>% 
-			dplyr::select(!!col_select, !!col_order) %>% 
-			dplyr::filter(!is.na(!!col_order)) %>% 
-			unique(.) %>% 
-			dplyr::arrange(!!col_order)		
+		corrplot_orders <- label_scheme_sub %>%
+			dplyr::select(!!col_select, !!col_order) %>%
+			dplyr::filter(!is.na(!!col_order)) %>%
+			unique(.) %>%
+			dplyr::arrange(!!col_order)
 
 		df$Intensity <- df$Intensity[, as.character(corrplot_orders[[col_select]]), drop = FALSE]
 		df$log2R <- df$log2R[, as.character(corrplot_orders[[col_select]]), drop = FALSE]
 	}
 
 	y_label <- x_label <- expression("Ratio ("*log[2]*")")
-	y_label_int <- x_label_int <- ifelse(use_log10, 
-	                                     expression("Intensity ("*log[10]*")"), "Intensity") 
-	
-	plot_corr_sub(df = df$Intensity, xlab = x_label_int, ylab = y_label_int, 
-	              filename = paste0(fn_prx, "_Intensity.png"), filepath = filepath, 
+	y_label_int <- x_label_int <- ifelse(use_log10,
+	                                     expression("Intensity ("*log[10]*")"), "Intensity")
+
+	plot_corr_sub(df = df$Intensity, xlab = x_label_int, ylab = y_label_int,
+	              filename = paste0(fn_prx, "_Intensity.png"), filepath = filepath,
 	              xmin = min_int, xmax = max_int, ...)
-	
-	plot_corr_sub(df = df$log2R, xlab = x_label, ylab = y_label, 
-	              filename = paste0(fn_prx, "_log2Ratio.png"), filepath = filepath, 
+
+	plot_corr_sub(df = df$log2R, xlab = x_label, ylab = y_label,
+	              filename = paste0(fn_prx, "_log2Ratio.png"), filepath = filepath,
 	              xmin = min_log2r, xmax = max_log2r, ...)
-} 
+}
 
 
 #' Make correlation plots
-#' 
+#'
 #' @import stringr dplyr ggplot2 GGally purrr rlang
 #' @importFrom magrittr %>%
 plot_corr_sub <- function (df, xlab, ylab, filename, filepath, xmin, xmax, ...) {
-  
+
   my_fn <- function(data, mapping, method = "lm", ...){
-    p <- ggplot(data = data, mapping = mapping) + 
-      geom_point(alpha = 0.3, size = .1) + 
+    p <- ggplot(data = data, mapping = mapping) +
+      geom_point(alpha = 0.3, size = .1) +
       geom_smooth(method = method, ...)
     p
   }
-  
+
   lm_with_cor <- function(data, mapping, ..., method = "pearson") {
     x <- data[[deparse(mapping$x)]]
     y <- data[[deparse(mapping$y)]]
     cor <- cor(x, y, method = method)
-    
+
     ggally_smooth_lm(data, mapping, ...) +
       ggplot2::geom_label(
         data = data.frame(
@@ -75,7 +75,7 @@ plot_corr_sub <- function (df, xlab, ylab, filename, filepath, xmin, xmax, ...) 
         size = 5, fontface = "bold"
       )
   }
-  
+
   panel_cor = function(x, y, digits = 2, prefix = "", cex.cor, ...){
     usr = par("usr")
     on.exit(par(usr))
@@ -87,7 +87,7 @@ plot_corr_sub <- function (df, xlab, ylab, filename, filepath, xmin, xmax, ...) 
     text(0.5, 0.5, txt, cex = cex.cor*(1+r)/2)
     bg = "transparent"
   }
-  
+
   panel_hist = function(x, ...){
     usr = par('usr')
     on.exit(par(usr))
@@ -100,151 +100,151 @@ plot_corr_sub <- function (df, xlab, ylab, filename, filepath, xmin, xmax, ...) 
     rect(breaks[-nB], 0, breaks[-1], y, col = 'white', ...)
     bg = "transparent"
   }
-  
+
   panel_lm = function(x, y, col = par('col'), bg = NA, pch = par('pch'),
                       cex = 1, col.smooth = 'black', ...){
     points(x, y, pch = pch, col = "red",  bg = bg, cex = cex)
     # abline(stats::lm(y~x), col=col.smooth,...)
     bg = "transparent"
   }
-  
+
   my_lower <- function(data, mapping, method = "lm", ...){
-    p <- ggplot(data = data, mapping = mapping) + 
-      geom_point(size = .02, alpha = .5) + 
-      geom_abline(alpha = .5, linetype = "dashed", color = "gray", size = 1) + 
+    p <- ggplot(data = data, mapping = mapping) +
+      geom_point(size = .02, alpha = .5) +
+      geom_abline(alpha = .5, linetype = "dashed", color = "gray", size = 1) +
       geom_smooth(size = 1, method = method, ...)
     p
   }
-  
+
   my_lower_no_sm <- function(data, mapping, method = "lm", ...){
-    p <- ggplot(data = data, mapping = mapping) + 
-      geom_point(size = .02, alpha = .5) + 
-      geom_abline(alpha = .5, linetype = "dashed", color = "gray", size = 1) 
+    p <- ggplot(data = data, mapping = mapping) +
+      geom_point(size = .02, alpha = .5) +
+      geom_abline(alpha = .5, linetype = "dashed", color = "gray", size = 1)
     p
   }
-  
+
   my_diag <- function(data, mapping, ...){
-    p <- ggplot(data = data, mapping = mapping) + 
+    p <- ggplot(data = data, mapping = mapping) +
       geom_density(fill = "#fec44f", size = .02, alpha = .5, adjust = 3)
     p
   }
-  
+
   my_custom_cor <- function(data, mapping, color = I("grey50"), sizeRange = c(1, 4), ...) {
     x <- GGally::eval_data_col(data, mapping$x)
     y <- GGally::eval_data_col(data, mapping$y)
-    
+
     ct <- cor.test(x, y)
     sig <- symnum(
       ct$p.value, corr = FALSE, na = FALSE,
       cutpoints = c(0, 0.001, 0.01, 0.05, 0.1, 1),
       symbols = c("***", "**", "*", ".", " ")
     )
-    
+
     r <- unname(ct$estimate)
     rt <- format(r, digits=2)[1]
-    
+
     cex <- max(sizeRange)
-    
+
     percent_of_range <- function(percent, range) {
       percent * diff(range) + min(range, na.rm = TRUE)
     }
-    
+
     ggally_text(
-      label = as.character(rt), 
+      label = as.character(rt),
       mapping = aes(),
-      xP = 0.5, yP = 0.5, 
+      xP = 0.5, yP = 0.5,
       size = I(percent_of_range(cex * abs(r), sizeRange)),
       color = color,
       ...
-    ) + 
+    ) +
       ## add the sig stars
       # geom_text(
       #   aes_string(
       #     x = 0.8,
       #     y = 0.8
       #   ),
-      #   label = sig, 
+      #   label = sig,
       #   size = I(cex),
       #   color = color,
       #   ...
-      # ) + 
-    theme_classic() + 
+      # ) +
+    theme_classic() +
       theme(
         panel.background = element_rect(
-          color = color, 
+          color = color,
           linetype = "longdash"
-        ), 
-        axis.line = element_blank(), 
-        axis.ticks = element_blank(), 
-        axis.text.y = element_blank(), 
+        ),
+        axis.line = element_blank(),
+        axis.ticks = element_blank(),
+        axis.text.y = element_blank(),
         axis.text.x = element_blank()
       )
   }
-  
+
   my_theme <- theme_bw() +
     theme(
       axis.text.x  = element_text(angle = 0, vjust = 0.5, size = 10),
       axis.text.y  = element_text(angle = 0, vjust = 0.5, size = 10),
       axis.title.x = element_text(colour = "black", size = 16),
       axis.title.y = element_text(colour = "black", size = 16),
-      plot.title = element_text(face = "bold", colour = "black", size = 20, 
+      plot.title = element_text(face = "bold", colour = "black", size = 20,
                                 hjust = 0.5, vjust = 0.5),
-      
-      panel.grid.major.x = element_blank(), 
+
+      panel.grid.major.x = element_blank(),
       panel.grid.minor.x = element_blank(),
       panel.grid.major.y = element_blank(),
-      panel.grid.minor.y = element_blank(), 
-      
-      legend.key = element_rect(colour = NA, fill = 'transparent'), 
+      panel.grid.minor.y = element_blank(),
+
+      legend.key = element_rect(colour = NA, fill = 'transparent'),
       legend.background = element_rect(colour = NA,  fill = "transparent"),
       legend.position = "none",
       legend.title = element_text(colour = "black", size = 16),
       legend.text = element_text(colour = "black", size = 14),
-      legend.text.align = 0, 
+      legend.text.align = 0,
       legend.box = NULL
     )
-  
+
   dots <- rlang::enexprs(...)
-  
+
   fn_prx <- gsub("\\..*$", "", filename)
-  
+
   n_TMT_sets <- n_TMT_sets(label_scheme)
-  
+
   if(is.null(dots$width)) {
     width <- 10*n_TMT_sets*1.2
   } else {
     width <- eval(dots$width, env = caller_env())
     dots$width <- NULL
   }
-  
+
   if(is.null(dots$height)) {
     height <- 10*n_TMT_sets*1.2
   } else {
     height <- eval(dots$height, env = caller_env())
     dots$height <- NULL
   }
-  
+
   if(is.null(dots$dpi)) {
     dpi <- 300
   } else {
     dpi <- eval(dots$dpi, env = caller_env())
     dots$dpi <- NULL
   }
-  
+
   png(file.path(filepath, filename), width = width, height = height, units = "in", res = 300)
     pairs(df, pch = ".", upper.panel = panel_cor, lower.panel = panel.smooth)
-  dev.off()	
-  
+  dev.off()
+
   ncol <- ncol(df)
-  
-  p1 <- ggpairs(df, columnLabels = as.character(names(df)), labeller = label_wrap_gen(10), 
-                title = "", xlab = xlab, ylab = ylab, lower = list(continuous = my_lower_no_sm), 
+
+  p1 <- ggpairs(df, columnLabels = as.character(names(df)), labeller = label_wrap_gen(10),
+                title = "", xlab = xlab, ylab = ylab, lower = list(continuous = my_lower_no_sm),
                 upper = list(continuous = my_custom_cor, digits = 2))
   p2 <- ggcorr(df, label = TRUE, label_round = 2)
-  
+
   g2 <- ggplotGrob(p2)
   colors <- g2$grobs[[6]]$children[[3]]$gp$fill
-  
+
   idx <- 1
   for (k1 in 1:(ncol-1)) { # row
     for (k2 in (k1+1):ncol) { # column
@@ -255,14 +255,14 @@ plot_corr_sub <- function (df, xlab, ylab, filename, filepath, xmin, xmax, ...) 
       idx <- idx + 1
     }
   }
-  
+
   idx <- 1
   for (k1 in 1:(ncol-1)) { # column
     for (k2 in (k1+1):ncol) { # row
       plt <- getPlot(p1, k2, k1) +
-        theme_bw() + 
-          theme(panel.background = element_rect(colour = NA,  fill = "transparent"), 
-                panel.grid.major.x = element_blank(), 
+        theme_bw() +
+          theme(panel.background = element_rect(colour = NA,  fill = "transparent"),
+                panel.grid.major.x = element_blank(),
                 panel.grid.minor.x = element_blank(),
                 panel.grid.major.y = element_blank(),
                 panel.grid.minor.y = element_blank()
@@ -271,11 +271,11 @@ plot_corr_sub <- function (df, xlab, ylab, filename, filepath, xmin, xmax, ...) 
       idx <- idx + 1
     }
   }
-  
+
   idx <- 1
   for (k1 in 1:ncol) { # row
     plt <- getPlot(p1, k1, k1) +
-      theme_bw() + 
+      theme_bw() +
         theme(panel.background = element_rect(colour = NA,  fill = "transparent"),
               panel.grid.major.x = element_blank(),
               panel.grid.minor.x = element_blank(),
@@ -285,24 +285,24 @@ plot_corr_sub <- function (df, xlab, ylab, filename, filepath, xmin, xmax, ...) 
     p1 <- putPlot(p1, plt, k1, k1)
     idx <- idx + 1
   }
-  
+
   for (x in 2:ncol) {
     for (y in 1:(x-1)) {
-      p1[x, y] <- p1[x, y] + 
-        scale_x_continuous(limits = c(xmin-.2, xmax+.2), breaks = c(xmin, 0, xmax)) + 
+      p1[x, y] <- p1[x, y] +
+        scale_x_continuous(limits = c(xmin-.2, xmax+.2), breaks = c(xmin, 0, xmax)) +
         scale_y_continuous(limits = c(xmin-.2, xmax+.2), breaks = c(xmin, 0, xmax))
     }
   }
-  
-  for (x in 1:ncol) 
-    p1[x, x] <- p1[x, x] + 
-      scale_x_continuous(limits = c(xmin-.2, xmax+.2), breaks = c(xmin, 0, xmax)) 
-  
-  p1 <- p1 + 
-    theme(plot.title = element_text(face = "bold", colour = "black", size = 20, 
+
+  for (x in 1:ncol)
+    p1[x, x] <- p1[x, x] +
+      scale_x_continuous(limits = c(xmin-.2, xmax+.2), breaks = c(xmin, 0, xmax))
+
+  p1 <- p1 +
+    theme(plot.title = element_text(face = "bold", colour = "black", size = 20,
                                     hjust = 0.5, vjust = 0.5))
-  
-  ggsave(file.path(filepath, paste0(fn_prx, "_gg.png")), p1, width = width, height = height, 
+
+  ggsave(file.path(filepath, paste0(fn_prx, "_gg.png")), p1, width = width, height = height,
          dpi = dpi, units = "in")
 }
 
@@ -344,11 +344,11 @@ plot_corr_sub <- function (df, xlab, ylab, filename, filepath, xmin, xmax, ...) 
 #'@import dplyr rlang ggplot2
 #'@importFrom magrittr %>%
 #'@export
-proteoCorrplot <- function (id = c("pep_seq", "pep_seq_mod", "prot_acc", "gene"), col_select = NULL, 
-                            col_order = NULL, use_log10 = TRUE, scale_log2r = FALSE, 
-														min_int = 3.5, max_int = 6.5, min_log2r = -2, max_log2r = 2, 
+proteoCorrplot <- function (id = c("pep_seq", "pep_seq_mod", "prot_acc", "gene"), col_select = NULL,
+                            col_order = NULL, use_log10 = TRUE, scale_log2r = FALSE,
+														min_int = 3.5, max_int = 6.5, min_log2r = -2, max_log2r = 2,
 														df = NULL, filepath = NULL, filename = NULL, ...) {
-	
+
   id <- rlang::enexpr(id)
 	if(length(id) != 1) id <- rlang::expr(gene)
 	stopifnot(rlang::as_string(id) %in% c("pep_seq", "pep_seq_mod", "prot_acc", "gene"))
@@ -356,17 +356,17 @@ proteoCorrplot <- function (id = c("pep_seq", "pep_seq_mod", "prot_acc", "gene")
 	col_select <- rlang::enexpr(col_select)
 	col_order <- rlang::enexpr(col_order)
 
-	info_anal(id = !!id, col_select = !!col_select, col_order = !!col_order, 
-	          scale_log2r = scale_log2r, impute_na = FALSE, 
-						df = df, filepath = filepath, filename = filename, 
-						anal_type = "Corrplot")(use_log10 = use_log10, min_int = min_int, 
-						                        max_int = max_int, min_log2r = min_log2r, 
+	info_anal(id = !!id, col_select = !!col_select, col_order = !!col_order,
+	          scale_log2r = scale_log2r, impute_na = FALSE,
+						df = df, filepath = filepath, filename = filename,
+						anal_type = "Corrplot")(use_log10 = use_log10, min_int = min_int,
+						                        max_int = max_int, min_log2r = min_log2r,
 						                        max_log2r = max_log2r, ...)
 }
 
 
 #'Correlation plots of peptide data
-#' 
+#'
 #'@seealso \code{\link{proteoCorrplot}} for parameters
 #'
 #' @examples
@@ -385,7 +385,7 @@ pepCorr <- function (...) {
 }
 
 #'Correlation plots of protein data
-#' 
+#'
 #'@seealso \code{\link{proteoCorrplot}} for parameters
 #'
 #' @examples
