18508b22ef97874061edba8e8aef7b5ddac4f5f4
qzhang503 <45829450+qzhang503@users.noreply.github.com>
2019-05-03 10:17:10 -0500

    Update proteoQ-params.R

--- R/proteoQ-params.R
+++ R/proteoQ-params.R
@@ -536,7 +536,7 @@ update_expts <- function (dat_dir = NULL, expt_smry = "expt_smry.xlsx", frac_smr
     dplyr::mutate(Accession_Type = acctype_sp[1, "Accession_Type"], Species = acctype_sp[1, "Species"])
   save(label_scheme, file = file.path(dat_dir, "label_scheme.Rdata"))
 
-  # load_dbs(dat_dir = dat_dir, expt_smry = expt_smry)
+  load_dbs(dat_dir = dat_dir, expt_smry = expt_smry)
 }
 
 
